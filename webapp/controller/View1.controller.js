f3e0ecebcc703bf4854a2a58cd4f841a1829271b
Sagar Salyan <sagarslyn@gmail.com>
2021-12-30 13:49:30 +0000

    initialcommit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,114 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ndc/BarcodeScanner",
+	"app/BarCode/controls/ExtScanner"
+], function (Controller, BarcodeScanner, ExtScanner) {
+	"use strict";
+
+	return Controller.extend("app.BarCode.controller.View1", {
+		onInit: function () {
+			// Zxing
+			this.oScanner = new ExtScanner({
+				settings: true,
+				valueScanned: this.onScanned.bind(this),
+				decoderKey: 'text',
+				decoders: this.getDecoders(),
+			});
+			// Zxing
+		},
+		onScanSuccess: function (oEvent) {
+
+		},
+		onScanError: function (oEvent) {
+
+		},
+		onScanUI5: function (oEvent) {
+			sap.ndc.BarcodeScanner.scan(
+				function (oSuccess) {
+
+				},
+				function (oError) {
+
+				}
+			);
+		},
+		onScanned: function (oEvent) {
+			this.oMainModel.setProperty('/scannedValue', oEvent.getParameter('value'));
+		},
+
+		onScan: function () {
+			this.oScanner.open();
+		},
+
+		getDecoders: function () {
+			return [{
+				key: 'PDF417-UII',
+				text: 'PDF417-UII',
+				decoder: this.parserPDF417UII,
+			}, {
+				key: 'text',
+				text: 'TEXT',
+				decoder: this.parserText,
+			}, ];
+		},
+
+		parserText: function (oResult) {
+			var sText = '';
+			var iLength = oResult.text.length;
+			for (var i = 0; i !== iLength; i++) {
+				if (oResult.text.charCodeAt(i) < 32) {
+					sText += ' ';
+				} else {
+					sText += oResult.text[i];
+				}
+			}
+			return sText;
+		},
+
+		parserPDF417UII: function (oResult) {
+			// we expect that
+			// first symbol of UII (S - ASCII = 83) or it just last group
+			var sText = oResult.text || '';
+			if (oResult.format && oResult.format === 10) {
+				sText = '';
+				var iLength = oResult.text.length;
+				var aChars = [];
+				for (var i = 0; i !== iLength; i++) {
+					aChars.push(oResult.text.charCodeAt(i));
+				}
+				var iStart = -1;
+				var iGRCounter = 0;
+				var iGroupUII = -1;
+				var sTemp = '';
+				aChars.forEach(function (code, k) {
+					switch (code) {
+					case 30:
+						if (iStart === -1) {
+							iStart = k;
+							sTemp = '';
+						} else {
+							sText = sTemp;
+							iGRCounter = -1;
+						}
+						break;
+					case 29:
+						iGRCounter += 1;
+						break;
+					default:
+						if (iGRCounter > 2 && code === 83 && iGRCounter > iGroupUII) {
+							sTemp = '';
+							iGroupUII = iGRCounter;
+						}
+						if (iGroupUII === iGRCounter) {
+							sTemp += String.fromCharCode(code);
+						}
+					}
+				});
+				if (sText) {
+					sText = sText.slice(1);
+				}
+			}
+			return sText;
+		},
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"app/BarCode/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
